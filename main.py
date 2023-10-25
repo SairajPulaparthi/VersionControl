bdbc4339d8aba3391d45669e8e9bb9188f321b88
SairajPulaparthi <saipulaparthi@Sais-MacBook-Air.local>
2023-10-25 14:26:14 -0400

    encoded

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#hi
+def encoder(password):
+    passWord = ''
+    for i in password:
+        number = int(i)
+        if number >= 7:
+            newNumber = number -7
+        else:
+            newNumber = number + 3
+        passWord += str(newNumber)
+    return  passWord
+
+
+def main():
+
+    print("Menu")
+    print("------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    option = input("Please enter an option: ")
+    password1 = input("Please enter your password to encode:")
+
+
