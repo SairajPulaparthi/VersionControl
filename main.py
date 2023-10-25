42644b13b990d6d96bd2f34541c8c2b43e10ce95
chrisokorr <c.okorochukwu@ufl.edu>
2023-10-25 14:34:36 -0400

    added decoder

--- main.py
+++ main.py
@@ -21,3 +21,13 @@ def main():
     password1 = input("Please enter your password to encode:")
 
 
+def decode(password):
+    result = ''
+    for i in password:
+        num = int(i)
+        if num >= 7:
+            new_num = num + 7
+        else:
+            new_num = num - 3
+        result += new_num
+    return result
