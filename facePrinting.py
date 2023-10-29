bd6539c5429b585d1b551f9c4926dc290d2787fc
SairajPulaparthi <saipulaparthi@Sais-Air.home>
2023-10-29 01:32:03 -0400

    added New file

---
+++ facePrinting.py
@@ -0,0 +1,14 @@
+nose = '0'  # Looks a little like a nose
+user_value = '-'
+
+while user_value != 'q':
+    print(f' {user_value} {user_value} ')  # Print eyes
+    print(f'  {nose}  ')  # Print nose
+    print(user_value*5)  # Print mouth
+    print('\n')
+
+    # Get new character for eyes and mouth
+    user_input = input("Enter a character ('q' for quit): ")
+    user_value = user_input[0]
+
+print('Goodbye.\n')
